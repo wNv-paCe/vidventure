13b1c42edc3952a937fe40ca982375f38e795fbd
wNv-paCe <wnv.pace@gmail.com>
2025-03-25 17:17:25 -0600

    Update route.js

--- app/api/stripe/webhook/route.js
+++ app/api/stripe/webhook/route.js
@@ -1,6 +1,7 @@
 import Stripe from "stripe";
 import { db } from "@/app/_utils/firebase";
 import { doc, runTransaction } from "firebase/firestore";
+import { buffer } from "micro";
 
 export const config = {
   api: {
@@ -11,8 +12,8 @@ export const config = {
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(req) {
-  const sig = req.headers.get("stripe-signature");
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  const sig = req.headers.get("stripe-signature");
 
   if (!sig || !endpointSecret) {
     return new Response("Missing Stripe signature or webhook secret", {
@@ -22,8 +23,8 @@ export async function POST(req) {
 
   let event;
   try {
-    const rawBody = await req.text();
-    event = stripe.webhooks.constructEvent(rawBody, sig, endpointSecret);
+    const buf = await buffer(req);
+    event = stripe.webhooks.constructEvent(buf, sig, endpointSecret);
   } catch (err) {
     return new Response(`Webhook Error: ${err.message}`, { status: 400 });
   }
